f7bc345ffa59dc94b5ee0d54d4b8a7f908e161a8
Juan Ignacio <juanyarbuh123@gmail.com>
2022-04-14 19:06:12 -0300

    build: makefile
    Co-authored-by: jlsuh <joelleonardosuh@gmail.com>

--- consola/settings.mk
+++ consola/settings.mk
@@ -10,7 +10,7 @@ CDEBUG=-g -Wall -DDEBUG
 CRELEASE=-O3 -Wall -DNDEBUG
 
 # Arguments when executing with start, memcheck or helgrind
-ARGS=
+ARGS= 500 /home/juani/Github/testfile
 
 # Valgrind flags
 MEMCHECK_FLAGS=--track-origins=yes --log-file="memcheck.log"
