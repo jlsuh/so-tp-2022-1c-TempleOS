32820a001727650c523e004aa3848b06b54f1856
jlsuh <joelleonardosuh@gmail.com>
2022-04-15 04:11:15 -0300

    test(consola_tests): consola_parser tests

--- consola_tests/src/consola_tests.c
+++ consola_tests/src/consola_tests.c
@@ -1,8 +1,35 @@
-#include <stdio.h>
-#include <stdbool.h>
 #include <CUnit/Basic.h>
+#include <stdbool.h>
+#include <stdio.h>
+
+#include "consola_parser_tests.h"
+#include "testing_utils.h"
+
+static void consola_parser_tests(void) {
+    CU_pSuite consolaParserSuite = CU_add_suite_with_setup_and_teardown(
+        "consola.h Test Suite",
+        NULL,
+        NULL,
+        test_consola_parser_setup,
+        test_consola_parser_tear_down);
+    t_test_case consolaParserTestCases[] = {
+        TEST_FUNC(test_es_posible_parsear_un_archivo_valido_de_instrucciones),
+        TEST_FUNC(test_es_posible_parsear_un_archivo_valido_de_instrucciones_con_lineas_vacias),
+        TEST_FUNC(test_no_es_posible_parsear_un_archivo_invalido_de_instrucciones_debido_a_la_primer_instruccion),
+        TEST_FUNC(test_no_es_posible_parsear_un_archivo_invalido_de_instrucciones_debido_a_la_ultima_instruccion),
+        TEST_FUNC(test_no_es_posible_parsear_un_archivo_invalido_de_instrucciones_con_lineas_vacias_debido_a_la_ultima_instruccion),
+    };
+    ADD_TEST_CASES_TO_SUITE(consolaParserSuite, consolaParserTestCases);
+}
 
 int main(int argc, char* argv[]) {
-    puts("Hello from consola_tests!!");
-    return 0;
+    CU_initialize_registry();
+    CU_basic_set_mode(CU_BRM_VERBOSE);
+
+    consola_parser_tests();
+
+    CU_basic_run_tests();
+    CU_cleanup_registry();
+
+    return CU_get_error();
 }
