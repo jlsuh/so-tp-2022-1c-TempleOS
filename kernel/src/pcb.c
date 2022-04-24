5f1a1c5db1fec958ddd30da692bab60694dcd61e
jlsuh <joelleonardosuh@gmail.com>
2022-04-24 16:59:10 -0300

    feat(kernel): pcb destroy

--- kernel/src/pcb.c
+++ kernel/src/pcb.c
@@ -4,6 +4,7 @@
 #include <stdint.h>
 
 #include "estados.h"
+#include "instruccion.h"
 
 struct t_pcb {
     uint32_t pid;
@@ -28,6 +29,11 @@ t_pcb* pcb_create(uint32_t pid, uint32_t tamanio, double estimacionInicial) {
     self->estadoActual = NEW;
     self->tiempoDeBloqueo = 0;
     return self;
+}
+
+void pcb_destroy(t_pcb* self) {
+    list_destroy_and_destroy_elements(self->instrucciones, instruccion_destroy);
+    free(self);
 }
 
 uint32_t pcb_get_pid(t_pcb* self) {
