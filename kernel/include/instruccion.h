a60d111479639dc94b7459943a6d6ffa21bb4f79
jlsuh <joelleonardosuh@gmail.com>
2022-04-24 16:58:41 -0300

    feat(kernel): instrucción destroy

--- kernel/include/instruccion.h
+++ kernel/include/instruccion.h
@@ -8,5 +8,6 @@
 typedef struct t_instruccion t_instruccion;
 
 t_instruccion* instruccion_create(t_tipo_instruccion tipoInstruccion, uint32_t operando1, uint32_t operando2);
+void instruccion_destroy(void* selfVoid);
 
 #endif
