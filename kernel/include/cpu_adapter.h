5df0f56873ac4d9ce2dc78221a8d74d74622150c
jlsuh <joelleonardosuh@gmail.com>
2022-04-27 10:32:28 -0300

    refactor(kernel): parametrización de config y logger

--- kernel/include/cpu_adapter.h
+++ kernel/include/cpu_adapter.h
@@ -7,8 +7,8 @@
 #include "kernel_config.h"
 #include "pcb.h"
 
-void cpu_adapter_enviar_pcb_a_cpu(t_pcb* pcbAEnviar, t_kernel_config*, t_log*); 
-t_pcb* cpu_adapter_recibir_pcb_de_cpu(t_pcb* pcbARecibir, t_kernel_config* kernelConfig, t_log* kernelLogger);
-void cpu_adapter_interrumpir_cpu(int);
+void cpu_adapter_enviar_pcb_a_cpu(t_pcb* pcbAEnviar, t_kernel_config*, t_log*);
+t_pcb* cpu_adapter_recibir_pcb_de_cpu(t_pcb* pcbARecibir, t_kernel_config*, t_log*);
+void cpu_adapter_interrumpir_cpu(t_kernel_config*, t_log*);
 
 #endif
--- kernel/src/cpu_adapter.c
+++ kernel/src/cpu_adapter.c
@@ -24,7 +24,7 @@ void cpu_adapter_enviar_pcb_a_cpu(t_pcb* pcbAEnviar, t_kernel_config* kernelConf
 }
 
 t_pcb* cpu_adapter_recibir_pcb_de_cpu(t_pcb* pcbARecibir, t_kernel_config* kernelConfig, t_log* kernelLogger) {
-    uint32_t pidRecibido = 0,  tiempoDeBloqActualizado = 0, pcActualizado = 0, tablaPagsActualizada = 0;
+    uint32_t pidRecibido = 0, tiempoDeBloqActualizado = 0, pcActualizado = 0, tablaPagsActualizada = 0;
 
     t_buffer* bufferPcb = buffer_create();
     stream_recv_buffer(kernel_config_get_socket_dispatch_cpu(kernelConfig), bufferPcb);
@@ -34,23 +34,22 @@ t_pcb* cpu_adapter_recibir_pcb_de_cpu(t_pcb* pcbARecibir, t_kernel_config* kerne
     buffer_unpack(bufferPcb, &tablaPagsActualizada, sizeof(tablaPagsActualizada));
     buffer_destroy(bufferPcb);
 
-    if(pidRecibido == pcb_get_pid(pcbARecibir)){
+    if (pidRecibido == pcb_get_pid(pcbARecibir)) {
         pcb_set_tiempo_de_bloq(pcbARecibir, tiempoDeBloqActualizado);
         pcb_set_program_counter(pcbARecibir, pcActualizado);
         pcb_set_tabla_pagina_primer_nivel(pcbARecibir, tablaPagsActualizada);
-    }else{
+    } else {
         log_error(kernelLogger, "Error al recibir PCB de CPU");
     }
     return pcbARecibir;
 }
 
-void cpu_adapter_interrumpir_cpu(int socketInterrupt){
-    stream_send_empty_buffer(socketInterrupt, INT_interrumpir_ejecucion);
-    uint8_t cpuResponse = stream_recv_header(socketCPUDispatch);
-    stream_recv_empty_buffer(socketCPUDispatch);
+void cpu_adapter_interrumpir_cpu(t_kernel_config* kernelConfig, t_log* kernelLogger) {
+    stream_send_empty_buffer(kernel_config_get_socket_interrupt_cpu(kernelConfig), INT_interrumpir_ejecucion);
+    uint8_t cpuResponse = stream_recv_header(kernel_config_get_socket_dispatch_cpu(kernelConfig));
+    stream_recv_empty_buffer(kernel_config_get_socket_dispatch_cpu(kernelConfig));
     if (cpuResponse != INT_cpu_interrumpida) {
         log_error(kernelLogger, "Error al interrumpir CPU");
     }
     log_info(kernelLogger, "CPU interrumpida");
-    return;
-} 
+}
