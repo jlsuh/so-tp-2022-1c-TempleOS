d424f353159c9f23ef63b6b3682499bd88647c8e
Juan Ignacio <juanyarbuh123@gmail.com>
2022-05-01 19:18:07 -0300

    fix/refactor(Memoria): Se arregla problemas de build y se hacen refactors

--- memoria/src/memoria.c
+++ memoria/src/memoria.c
@@ -37,101 +37,38 @@ typedef struct
 t_log* memoriaLogger;
 t_memoria_config* memoriaConfig;
 void* memoriaPrincipal;
-t_tabla_nivel_uno* tablasNivel1;
-t_tabla_nivel_dos* tablasNivel2;
+t_tabla_nivel_uno* tablasDeNivel1;
+t_tabla_nivel_dos* tablasDeNivel2;
 bool* marcosEnUso;
 int cantTotalMarcos;
+int marcosPorProceso;
 int cantidadProcesosMax;
 int entradasPorTabla;
 int tamanioPagina;
-int marcosPorProceso;
 
+int crear_tabla_de_nivel_2(void);
 int main(int argc, char* argv[]);
-int obtener_marco_disponible(void);
-int obtener_tabla_primer_nivel_libre(void);
-int obtener_tabla_segundo_nivel_libre(void);
-int crear_tabla_segundo_nivel(void);
+int obtener_marco(uint32_t nroDeTabla1, uint32_t nroDeTabla2, uint32_t entradaDeTabla2);
+int obtener_marco_libre(void);
+int obtener_tabla_libre_de_nivel_1(void);
+int obtener_tabla_libre_de_nivel_2(void);
 uint32_t crear_nuevo_proceso(uint32_t tamanio);
 void* escuchar_peticiones_cpu(void);
 void* escuchar_peticiones_kernel(void);
-void asignar_marcos_a_proceso(int numeroTablaPrimerNivel, int indiceMarco);
-void crear_tablas_de_segundo_nivel(int numeroTablaPrimerNivel);
-void inicializar_array_de_tablas_de_primer_nivel(void);
-void inicializar_array_de_tablas_de_segundo_nivel(void);
-void inicializar_marcos(void);
 void* recibir_conexion(int socketEscucha, pthread_t* threadSuscripcion);
+void asignar_marcos_a_proceso(int nroTablaNivel1, int indiceMarco);
+void crear_tablas_de_nivel_2(int nroTablaNivel1);
+void dar_marco_cpu(int socket, t_buffer* buffer);
+void imprimir_frames(void);
+void imprimir_tabla_nivel_dos(void);
+void imprimir_tabla_nivel_uno(void);
+void inicializar_marcos(void);
+void inicializar_tablas_de_nivel_1(void);
+void inicializar_tablas_de_nivel_2(void);
 void recibir_conexiones(int socketEscucha);
+void imprimir_memoria(void);
 
-void imprimir_tabla_nivel_uno(void) {
-    printf("\n");
-    for (int i = 0; i < cantidadProcesosMax; i++) {
-        printf("Tabla %d: [ ", i);
-        for (int j = 0; j < entradasPorTabla; j++) {
-            printf("%d ", tablasNivel1[i].nroTablaNivel2[j]);
-        }
-        printf("]\n");
-    }
-    printf("\n");
-}
-void imprimir_tabla_nivel_dos(void) {
-    printf("\n");
-    for (int i = 0; i < cantidadProcesosMax; i++) {
-        printf("Tabla %d\n", i);
-        for (int j = 0; j < entradasPorTabla; j++) {
-            printf("Tabla Nivel Dos %d\n", tablasNivel1[i].nroTablaNivel2[j]);
-            int numerosTablaNivelDos = tablasNivel1[i].nroTablaNivel2[j];
-            for (int r = 0; r < entradasPorTabla; r++) {
-                printf("[ %d, %d, %d, %d ]\n", tablasNivel2[numerosTablaNivelDos].entradaNivel2[r].indiceMarco, tablasNivel2[numerosTablaNivelDos].entradaNivel2[r].bitPresencia, tablasNivel2[numerosTablaNivelDos].entradaNivel2[r].bitUso, tablasNivel2[numerosTablaNivelDos].entradaNivel2[r].bitModificado);
-            }
-        }
-        printf("\n\n\n");
-    }
-    printf("\n");
-}
-void imprimir_frames(void) {
-    printf("\n");
-    for (int i = 0; i < cantTotalMarcos; i++) {
-        printf("Marco %d: %d\n", i, marcosEnUso[i]);
-    }
-    printf("\n");
-}
 
-int obtener_marco(uint32_t nroDeTabla1, uint32_t nroDeTabla2, uint32_t entradaDeTabla2) {
-    int marco = tablasNivel2[nroDeTabla2].entradaNivel2[entradaDeTabla2].indiceMarco;
-    if (marco != -1) {
-        return marco;
-    }
-
-    // TODO swap con algoritmo
-    return -1;
-}
-void dar_marco_cpu(int socket, t_buffer* buffer) {
-    uint32_t nroDeTabla1, entradaDeTabla1;
-
-    buffer_unpack(buffer, &nroDeTabla1, sizeof(nroDeTabla1));
-    buffer_unpack(buffer, &entradaDeTabla1, sizeof(entradaDeTabla1));
-    buffer_destroy(buffer);
-
-    uint32_t nroDeTabla2 = tablasNivel1[nroDeTabla1].nroTablaNivel2[entradaDeTabla1];
-    t_buffer* buffer_rta = buffer_create();
-    buffer_pack(buffer_rta, &nroDeTabla2, sizeof(nroDeTabla2));
-    stream_send_buffer(socket, HEADER_rta_tabla_segundo_nivel, buffer_rta);
-    buffer_destroy(buffer_rta);
-
-    uint32_t header = stream_recv_header(socket);
-    stream_recv_buffer(socket, buffer);
-    uint32_t entradaDeTabla2;
-    if (header == HEADER_solicitud_marco) {
-        buffer_unpack(buffer, &nroDeTabla2, sizeof(nroDeTabla2));
-        buffer_unpack(buffer, &entradaDeTabla2, sizeof(entradaDeTabla2));
-        buffer_destroy(buffer);
-    } 
-    int indiceMarco = obtener_marco(nroDeTabla1, nroDeTabla2, entradaDeTabla2);
-    uint32_t marco = indiceMarco * tamanioPagina;
-    buffer_pack(buffer_rta, &marco, sizeof(marco));
-    stream_send_buffer(socket, HEADER_rta_marco, buffer_rta);
-    buffer_destroy(buffer_rta);
-}
 
 int main(int argc, char* argv[]) {
     memoriaLogger = log_create(MEMORIA_LOG_PATH, MEMORIA_MODULE_NAME, true, LOG_LEVEL_INFO);
@@ -150,17 +87,9 @@ int main(int argc, char* argv[]) {
     memoriaPrincipal = malloc(tamanioMemoria);
     memset(memoriaPrincipal, 0, tamanioMemoria);
 
-    inicializar_array_de_tablas_de_primer_nivel();
-    inicializar_array_de_tablas_de_segundo_nivel();
+    inicializar_tablas_de_nivel_1();
+    inicializar_tablas_de_nivel_2();
     inicializar_marcos();
-
-    crear_nuevo_proceso(5);
-    crear_nuevo_proceso(20);
-    crear_nuevo_proceso(34);
-
-    imprimir_frames();
-    imprimir_tabla_nivel_uno();
-    imprimir_tabla_nivel_dos();
 
     recibir_conexiones(socketEscucha);
 
@@ -291,8 +220,46 @@ void* recibir_conexion(int socketEscucha, pthread_t* threadSuscripcion) {
     return funcion_suscripcion;
 }
 
+int obtener_marco(uint32_t nroDeTabla1, uint32_t nroDeTabla2, uint32_t entradaDeTabla2) {
+    int marco = tablasDeNivel2[nroDeTabla2].entradaNivel2[entradaDeTabla2].indiceMarco;
+    if (marco != -1) {
+        return marco;
+    }
+
+    // TODO swap con algoritmo
+    return -1;
+}
+
+void dar_marco_cpu(int socket, t_buffer* buffer) {
+    uint32_t nroDeTabla1, entradaDeTabla1;
+
+    buffer_unpack(buffer, &nroDeTabla1, sizeof(nroDeTabla1));
+    buffer_unpack(buffer, &entradaDeTabla1, sizeof(entradaDeTabla1));
+    buffer_destroy(buffer);
+
+    uint32_t nroDeTabla2 = tablasDeNivel1[nroDeTabla1].nroTablaNivel2[entradaDeTabla1];
+    t_buffer* buffer_rta = buffer_create();
+    buffer_pack(buffer_rta, &nroDeTabla2, sizeof(nroDeTabla2));
+    stream_send_buffer(socket, HEADER_rta_tabla_segundo_nivel, buffer_rta);
+    buffer_destroy(buffer_rta);
+
+    uint32_t header = stream_recv_header(socket);
+    stream_recv_buffer(socket, buffer);
+    uint32_t entradaDeTabla2;
+    if (header == HEADER_solicitud_marco) {
+        buffer_unpack(buffer, &nroDeTabla2, sizeof(nroDeTabla2));
+        buffer_unpack(buffer, &entradaDeTabla2, sizeof(entradaDeTabla2));
+        buffer_destroy(buffer);
+    }
+    int indiceMarco = obtener_marco(nroDeTabla1, nroDeTabla2, entradaDeTabla2);
+    uint32_t marco = indiceMarco * tamanioPagina;
+    buffer_pack(buffer_rta, &marco, sizeof(marco));
+    stream_send_buffer(socket, HEADER_rta_marco, buffer_rta);
+    buffer_destroy(buffer_rta);
+}
+
 uint32_t crear_nuevo_proceso(uint32_t tamanio) {
-    int indiceMarco = obtener_marco_disponible();
+    int indiceMarco = obtener_marco_libre();
     int tamanioMaximo = entradasPorTabla * entradasPorTabla * tamanioPagina;
     if (indiceMarco == -1) {
         return -1;
@@ -300,96 +267,153 @@ uint32_t crear_nuevo_proceso(uint32_t tamanio) {
         return -1;
     }
 
-    int numeroTablaPrimerNivel = obtener_tabla_primer_nivel_libre();
-    crear_tablas_de_segundo_nivel(numeroTablaPrimerNivel);
-    asignar_marcos_a_proceso(numeroTablaPrimerNivel, indiceMarco);
+    int nroTablaNivel1 = obtener_tabla_libre_de_nivel_1();
+    crear_tablas_de_nivel_2(nroTablaNivel1);
+    asignar_marcos_a_proceso(nroTablaNivel1, indiceMarco);
 
     // TODO crear estructura para swap
 
-    return numeroTablaPrimerNivel;
+    return nroTablaNivel1;
 }
 
-void asignar_marcos_a_proceso(int numeroTablaPrimerNivel, int indiceMarco) {
-    int cantidadMarcosRestantes = marcosPorProceso;
+void asignar_marcos_a_proceso(int nroTablaNivel1, int indiceMarco) {
+    int cantMarcosRestantes = marcosPorProceso;
     int indiceMarcoActual = indiceMarco;
     for (int i = 0; i < entradasPorTabla; i++) {
         for (int j = 0; j < entradasPorTabla; j++) {
-            int nroTablaNivel2 = tablasNivel1[numeroTablaPrimerNivel].nroTablaNivel2[i];
-            tablasNivel2[nroTablaNivel2].entradaNivel2[j].indiceMarco = indiceMarcoActual;
-            tablasNivel2[nroTablaNivel2].entradaNivel2[j].bitPresencia = true;
+            int nroTablaNivel2 = tablasDeNivel1[nroTablaNivel1].nroTablaNivel2[i];
+            tablasDeNivel2[nroTablaNivel2].entradaNivel2[j].indiceMarco = indiceMarcoActual;
+            tablasDeNivel2[nroTablaNivel2].entradaNivel2[j].bitPresencia = true;
 
-            cantidadMarcosRestantes--;
+            cantMarcosRestantes--;
             marcosEnUso[indiceMarcoActual++] = true;
 
-            if (cantidadMarcosRestantes == 0) {
+            if (cantMarcosRestantes == 0) {
                 return;
             }
         }
     }
 }
 
-int obtener_tabla_primer_nivel_libre(void) {
-    int i;
-    for (i = 0; i < cantidadProcesosMax; i++) {
-        if (tablasNivel1[i].nroTablaNivel2[0] == -1) {
-            break;
-        }
-    }
-    return i;
-}
-
-void crear_tablas_de_segundo_nivel(int numeroTablaPrimerNivel) {
+void crear_tablas_de_nivel_2(int nroTablaNivel1) {
     for (int i = 0; i < entradasPorTabla; i++) {
-        int indiceTablaSegundoNivel = crear_tabla_segundo_nivel();
-        tablasNivel1[numeroTablaPrimerNivel].nroTablaNivel2[i] = indiceTablaSegundoNivel;
+        int indiceTablaSegundoNivel = crear_tabla_de_nivel_2();
+        tablasDeNivel1[nroTablaNivel1].nroTablaNivel2[i] = indiceTablaSegundoNivel;
     }
 }
 
-int crear_tabla_segundo_nivel(void) {
-    int tablaSegundoNivelLibre = obtener_tabla_segundo_nivel_libre();
-    tablasNivel2[tablaSegundoNivelLibre].entradaNivel2 = malloc(entradasPorTabla * sizeof(t_entrada_nivel_dos));
+int crear_tabla_de_nivel_2(void) {
+    int tablaSegundoNivelLibre = obtener_tabla_libre_de_nivel_2();
+    tablasDeNivel2[tablaSegundoNivelLibre].entradaNivel2 = malloc(entradasPorTabla * sizeof(t_entrada_nivel_dos));
     for (int i = 0; i < entradasPorTabla; i++) {
-        tablasNivel2[tablaSegundoNivelLibre].entradaNivel2[i] =
+        tablasDeNivel2[tablaSegundoNivelLibre].entradaNivel2[i] =
             (t_entrada_nivel_dos){.indiceMarco = -1, .bitPresencia = false, .bitUso = false, .bitModificado = false};
     }
 
     return tablaSegundoNivelLibre;
 }
 
-int obtener_tabla_segundo_nivel_libre(void) {
+int obtener_tabla_libre_de_nivel_1(void) {
     int i;
-    for (i = 0; i < cantidadProcesosMax * entradasPorTabla; i++) {
-        if (tablasNivel2[i].entradaNivel2 == NULL) {
+    for (i = 0; i < cantidadProcesosMax; i++) {
+        if (tablasDeNivel1[i].nroTablaNivel2[0] == -1) {
             break;
         }
     }
     return i;
 }
 
-void inicializar_array_de_tablas_de_primer_nivel(void) {
-    tablasNivel1 = malloc(cantidadProcesosMax * sizeof(t_tabla_nivel_uno));
-
-    for (int i = 0; i < cantidadProcesosMax; i++) {
-        tablasNivel1[i].nroTablaNivel2 = malloc(entradasPorTabla * sizeof(int));
-        for (int j = 0; j < entradasPorTabla; j++) {
-            tablasNivel1[i].nroTablaNivel2[j] = -1;
+int obtener_tabla_libre_de_nivel_2(void) {
+    int i;
+    for (i = 0; i < cantidadProcesosMax * entradasPorTabla; i++) {
+        if (tablasDeNivel2[i].entradaNivel2 == NULL) {
+            break;
         }
     }
+    return i;
 }
 
-void inicializar_array_de_tablas_de_segundo_nivel(void) {
-    tablasNivel2 = calloc(cantidadProcesosMax * entradasPorTabla, sizeof(t_tabla_nivel_dos));
-}
-
-void inicializar_marcos(void) {
-    marcosEnUso = calloc(cantTotalMarcos, sizeof(bool));
-}
-
-int obtener_marco_disponible(void) {
+int obtener_marco_libre(void) {
     for (int i = 0; i < cantTotalMarcos; i++) {
         if (!marcosEnUso[i]) {
             return i;
         }
     }
     return -1;
+}
+
+void inicializar_tablas_de_nivel_1(void) {
+    tablasDeNivel1 = malloc(cantidadProcesosMax * sizeof(t_tabla_nivel_uno));
+
+    for (int i = 0; i < cantidadProcesosMax; i++) {
+        tablasDeNivel1[i].nroTablaNivel2 = malloc(entradasPorTabla * sizeof(int));
+        for (int j = 0; j < entradasPorTabla; j++) {
+            tablasDeNivel1[i].nroTablaNivel2[j] = -1;
+        }
+    }
+}
+
+void inicializar_tablas_de_nivel_2(void) {
+    tablasDeNivel2 = calloc(cantidadProcesosMax * entradasPorTabla, sizeof(t_tabla_nivel_dos));
+}
+
+void inicializar_marcos(void) {
+    marcosEnUso = calloc(cantTotalMarcos, sizeof(bool));
+}
+
+// Funciones de trackeo
+
+void imprimir_tabla_nivel_uno(void) {
+    printf("\n");
+    for (int i = 0; i < cantidadProcesosMax; i++) {
+        printf("Tabla %d: [ ", i);
+        for (int j = 0; j < entradasPorTabla; j++) {
+            printf("%d ", tablasDeNivel1[i].nroTablaNivel2[j]);
+        }
+        printf("]\n");
+    }
+    printf("\n");
+}
+
+void imprimir_tabla_nivel_dos(void) {
+    printf("\n");
+    for (int i = 0; i < cantidadProcesosMax; i++) {
+        printf("Tabla %d\n", i);
+        for (int j = 0; j < entradasPorTabla; j++) {
+            printf("Tabla Nivel Dos %d\n", tablasDeNivel1[i].nroTablaNivel2[j]);
+            int numerosTablaNivelDos = tablasDeNivel1[i].nroTablaNivel2[j];
+            for (int r = 0; r < entradasPorTabla; r++) {
+                printf("[ %d, %d, %d, %d ]\n", tablasDeNivel2[numerosTablaNivelDos].entradaNivel2[r].indiceMarco, tablasDeNivel2[numerosTablaNivelDos].entradaNivel2[r].bitPresencia, tablasDeNivel2[numerosTablaNivelDos].entradaNivel2[r].bitUso, tablasDeNivel2[numerosTablaNivelDos].entradaNivel2[r].bitModificado);
+            }
+        }
+        printf("\n\n\n");
+    }
+    printf("\n");
+}
+
+void imprimir_frames(void) {
+    printf("\n");
+    for (int i = 0; i < cantTotalMarcos; i++) {
+        printf("Marco %d: %d\n", i, marcosEnUso[i]);
+    }
+    printf("\n");
+}
+
+void imprimir_memoria(void){
+    uint32_t valor = 2561;
+    uint32_t valor2 = 423;
+    uint32_t valor3 = 543;
+    uint32_t valor4 = 6540;
+    uint32_t valor5 = 12;
+    memcpy(memoriaPrincipal + tamanioPagina * 5, &valor, sizeof(uint32_t));
+    memcpy(memoriaPrincipal + tamanioPagina * 10, &valor2, sizeof(uint32_t));
+    memcpy(memoriaPrincipal + tamanioPagina * 23, &valor3, sizeof(uint32_t));
+    memcpy(memoriaPrincipal + tamanioPagina * 2, &valor4, sizeof(uint32_t));
+    memcpy(memoriaPrincipal, &valor5, sizeof(uint32_t));
+
+    printf("\n");
+    for (int i = 0; i < cantTotalMarcos; i++) {
+        printf("Marco %d: con valor %d\n", i, *((uint32_t *)(memoriaPrincipal + i * tamanioPagina)));
+    }
+    printf("\n");
 }
