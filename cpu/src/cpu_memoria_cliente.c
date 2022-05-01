43e736bffc4a496b9995f622d251247be5f56833
Juan Ignacio <juanyarbuh123@gmail.com>
2022-05-01 11:45:04 -0300

    feat(CPU): Se crean los envios de peticion a memoria

--- cpu/src/cpu_memoria_cliente.c
+++ cpu/src/cpu_memoria_cliente.c
@@ -21,6 +21,49 @@ void escribir_en_memoria(uint32_t direccion, uint32_t nroDeTabla1, uint32_t valo
     buffer_pack(buffer, &marcoSend, sizeof(marcoSend));
     buffer_pack(buffer, &valor, sizeof(valor));
     stream_send_buffer(socket, HEADER_write, buffer);
+    buffer_destroy(buffer);
+}
+
+uint32_t leer_en_memoria(uint32_t direccion, uint32_t nroDeTabla1) {
+    int marco = obtener_marco(direccion, nroDeTabla1);
+    if (marco == -1) {  // TODO
+        printf("No se pudo escribir en memoria\n");
+        return;
+    }
+
+    t_buffer *buffer = buffer_create();
+    uint32_t marcoSend = marco;
+    buffer_pack(buffer, &marcoSend, sizeof(marcoSend));
+    stream_send_buffer(socket, HEADER_write, buffer);
+    buffer_destroy(buffer);
+
+    uint32_t header = stream_recv_header(socket);
+    if (header != HEADER_read) {
+        printf("Error al leer en memoria\n");
+        return -1;
+    }
+    t_buffer *bufferRecv = buffer_create();
+    uint32_t valor;
+    buffer_unpack(bufferRecv, &valor, sizeof(valor));
+    buffer_destroy(bufferRecv);
+    return valor;
+}
+
+void copiar_en_memoria(uint32_t direccionDestino, uint32_t direccionOrigen, uint32_t nroDeTabla1) {
+    int marcoDestino = obtener_marco(direccionDestino, nroDeTabla1);
+    int marcoOrigen = obtener_marco(direccionOrigen, nroDeTabla1);
+    if (marcoDestino == -1 || marcoOrigen == -1) {  // TODO
+        printf("No se pudo copiar a memoria\n");
+        return;
+    }
+
+    t_buffer *buffer = buffer_create();
+    uint32_t marcoDestSend = marcoDestino;
+    uint32_t marcoOrigenSend = marcoOrigen;
+    buffer_pack(buffer, &marcoDestSend, sizeof(marcoDestSend));
+    buffer_pack(buffer, &marcoOrigenSend, sizeof(marcoOrigenSend));
+    stream_send_buffer(socket, HEADER_copy, buffer);
+    buffer_destroy(buffer);
 }
 
 int obtener_marco(uint32_t direccion, uint32_t nroDeTabla1) {
@@ -35,7 +78,6 @@ int obtener_marco(uint32_t direccion, uint32_t nroDeTabla1) {
     uint32_t entradaTablaNivel2 = nroPag % entradasPorTabla;
     uint32_t offset = direccion - nroPag * tamPagina;
 
-    
     t_buffer *bufferSolicitud = buffer_create();
     buffer_pack(bufferSolicitud, &nroDeTabla1, sizeof(nroDeTabla1));
     buffer_pack(bufferSolicitud, &entradaTablaNivel1, sizeof(entradaTablaNivel1));
