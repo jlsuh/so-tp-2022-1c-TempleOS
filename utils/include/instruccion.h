ddd9842bc08fe2713e5f9d5eae0100025f192574
jlsuh <joelleonardosuh@gmail.com>
2022-05-03 02:09:22 -0300

    refactor(utils): getters & setters

--- utils/include/instruccion.h
+++ utils/include/instruccion.h
@@ -13,5 +13,8 @@ typedef struct t_instruccion t_instruccion;
 t_instruccion* instruccion_create(t_tipo_instruccion tipoInstruccion, uint32_t operando1, uint32_t operando2);
 void instruccion_destroy(void* selfVoid);
 t_list* instruccion_list_create_from_buffer(t_buffer* bufferConInstrucciones, t_log*);
+t_tipo_instruccion instruccion_get_tipo_instruccion(t_instruccion*);
+uint32_t instruccion_get_operando1(t_instruccion*);
+uint32_t instruccion_get_operando2(t_instruccion*);
 
 #endif
