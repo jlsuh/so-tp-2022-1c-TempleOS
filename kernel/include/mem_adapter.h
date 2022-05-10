bf618ed768230a45c719a36326b4ac7ca984bf4e
jlsuh <joelleonardosuh@gmail.com>
2022-05-10 17:18:52 -0300

    refactor(kernel): #16
    Co-authored-by: matiaspar2000 <matipartarrie@gmail.com>

--- kernel/include/mem_adapter.h
+++ kernel/include/mem_adapter.h
@@ -7,7 +7,7 @@
 #include "kernel_config.h"
 #include "pcb.h"
 
-uint32_t mem_adapter_obtener_tabla_pagina(t_pcb* pcbAIniciar, t_kernel_config*, t_log*);
+int mem_adapter_obtener_tabla_pagina(t_pcb* pcbAIniciar, t_kernel_config*, t_log*);
 void mem_adapter_finalizar_proceso(t_pcb* pcbAFinalizar, t_kernel_config* kernelConfig, t_log* kernelLogger);
 
 #endif
