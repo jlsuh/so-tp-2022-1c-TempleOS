a57013716503e0c2784f887b8d9bccad84579af4
jlsuh <joelleonardosuh@gmail.com>
2022-05-24 15:08:35 -0300

    chore(consola): instrucciones

--- consola/settings.mk
+++ consola/settings.mk
@@ -10,7 +10,7 @@ CDEBUG=-g -Wall -DDEBUG
 CRELEASE=-O3 -Wall -DNDEBUG
 
 # Arguments when executing with start, memcheck or helgrind
-ARGS=500 ./instrucciones/archivoExcedeTiempoBloqueoMaximo
+ARGS=500 ./instrucciones/archivo2
 
 # Valgrind flags
 MEMCHECK_FLAGS=--track-origins=yes --log-file="memcheck.log"
