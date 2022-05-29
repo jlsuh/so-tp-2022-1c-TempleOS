3f1357b78c992bbab0cb92dfc96a78dae9e98e7a
jlsuh <joelleonardosuh@gmail.com>
2022-05-29 00:41:11 -0300

    refactor(kernel): reordenar interfaz

--- kernel/include/scheduler.h
+++ kernel/include/scheduler.h
@@ -7,13 +7,16 @@
 
 #include "estado.h"
 
-bool pcb_es_este_pcb_por_pid(void* unPcb, void* otroPcb);
+typedef t_pcb* (*t_algoritmo)(t_estado*, double);
+
 double calcular_siguiente_estimacion(t_pcb* pcb, double alfa);
 double media_exponencial(double alfa, double realAnterior, double estimacionAnterior);
-t_pcb* elegir_pcb_segun_fifo(t_estado* estado, double _);
-t_pcb* elegir_pcb_segun_srt(t_estado* estado, double alfa);
 t_pcb* menor_rafaga_restante(t_pcb* unPcb, t_pcb* otroPcb);
+t_pcb* segun_fifo(t_estado* estado, double _);
+t_pcb* segun_srt(t_estado* estado, double alfa);
 void* encolar_en_new_a_nuevo_pcb_entrante(void* socket);
+void actualizar_pcb_por_bloqueo(t_pcb* pcb, double realEjecutado);
+void actualizar_pcb_por_desalojo(t_pcb* pcb, double realEjecutado);
 void inicializar_estructuras(void);
 
 #endif
--- kernel/src/estado.c
+++ kernel/src/estado.c
@@ -14,6 +14,30 @@ struct t_estado {
     pthread_mutex_t* mutexEstado;
 };
 
+t_estado* estado_create(t_nombre_estado nombre) {
+    t_estado* self = malloc(sizeof(*self));
+    self->listaProcesos = list_create();
+    self->mutexEstado = malloc(sizeof(*(self->mutexEstado)));
+    pthread_mutex_init(self->mutexEstado, NULL);
+    self->nombreEstado = nombre;
+    self->semaforoEstado = malloc(sizeof(*(self->semaforoEstado)));
+    sem_init(self->semaforoEstado, 0, 0);
+    return self;
+}
+
+void estado_destroy(t_estado* self) {
+    if (list_is_empty(self->listaProcesos)) {
+        list_destroy(self->listaProcesos);
+    } else {
+        list_destroy_and_destroy_elements(self->listaProcesos, (void*)pcb_destroy);
+    }
+    pthread_mutex_destroy(self->mutexEstado);
+    sem_destroy(self->semaforoEstado);
+    free(self->semaforoEstado);
+    free(self->mutexEstado);
+    free(self);
+}
+
 t_pcb* estado_remover_pcb_de_cola(t_estado* self, t_pcb* targetPcb) {
     t_pcb* pcb = NULL;
     uint32_t index = list_get_index(estado_get_list(self), pcb_es_este_pcb_por_pid, targetPcb);
@@ -50,17 +74,6 @@ t_pcb* estado_desencolar_primer_pcb_atomic(t_estado* self) {
     t_pcb* pcb = estado_desencolar_primer_pcb(self);
     pthread_mutex_unlock(estado_get_mutex(self));
     return pcb;
-}
-
-t_estado* estado_create(t_nombre_estado nombre) {
-    t_estado* self = malloc(sizeof(*self));
-    self->listaProcesos = list_create();
-    self->mutexEstado = malloc(sizeof(*(self->mutexEstado)));
-    pthread_mutex_init(self->mutexEstado, NULL);
-    self->nombreEstado = nombre;
-    self->semaforoEstado = malloc(sizeof(*(self->semaforoEstado)));
-    sem_init(self->semaforoEstado, 0, 0);
-    return self;
 }
 
 void estado_encolar_pcb_atomic(t_estado* estadoDest, t_pcb* targetPcb) {
