0bd8364af0fee2607e66d7a0c28f1b1030395eec
jlsuh <joelleonardosuh@gmail.com>
2022-06-04 20:16:09 -0300

    refactor(cpu): programCounter como uint32_t

--- cpu/src/cpu_pcb.c
+++ cpu/src/cpu_pcb.c
@@ -6,12 +6,12 @@
 
 struct t_cpu_pcb {
     uint32_t pid;
-    uint64_t programCounter;
+    uint32_t programCounter;
     uint32_t tablaPaginaPrimerNivel;
     t_list* instrucciones;
 };
 
-t_cpu_pcb* cpu_pcb_create(uint32_t pid, uint64_t programCounter, uint32_t tablaPaginaPrimerNivel) {
+t_cpu_pcb* cpu_pcb_create(uint32_t pid, uint32_t programCounter, uint32_t tablaPaginaPrimerNivel) {
     t_cpu_pcb* self = malloc(sizeof(*self));
     self->pid = pid;
     self->programCounter = programCounter;
@@ -24,12 +24,12 @@ uint32_t cpu_pcb_get_pid(t_cpu_pcb* self) {
     return self->pid;
 }
 
-uint64_t cpu_pcb_get_program_counter(t_cpu_pcb* self) {
+uint32_t cpu_pcb_get_program_counter(t_cpu_pcb* self) {
     return self->programCounter;
 }
 
-void cpu_pcb_set_program_counter(t_cpu_pcb* self, uint64_t pc) {
-    self->programCounter = pc;
+void cpu_pcb_set_program_counter(t_cpu_pcb* self, uint32_t programCounter) {
+    self->programCounter = programCounter;
 }
 
 uint32_t cpu_pcb_get_tabla_pagina_primer_nivel(t_cpu_pcb* self) {
