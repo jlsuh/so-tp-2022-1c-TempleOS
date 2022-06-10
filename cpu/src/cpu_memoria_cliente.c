05ee18a6e70c4ded148e4caed4a02e194274f83d
jlsuh <joelleonardosuh@gmail.com>
2022-06-10 19:16:51 -0300

    refactor(cpu): mejorando expresividad & deprecando copoiar_en_memoria

--- cpu/src/cpu_memoria_cliente.c
+++ cpu/src/cpu_memoria_cliente.c
@@ -9,83 +9,72 @@
 extern t_cpu_config *cpuConfig;
 extern t_log *cpuLogger;
 
-uint32_t solicitar(int toSocket, uint32_t nroDeTabla, uint32_t entradaDeTabla, t_header header) {
-    t_buffer *bufferSolicitud = buffer_create();
-    buffer_pack(bufferSolicitud, &nroDeTabla, sizeof(nroDeTabla));
-    buffer_pack(bufferSolicitud, &entradaDeTabla, sizeof(entradaDeTabla));
-    stream_send_buffer(toSocket, header, bufferSolicitud);
-    buffer_destroy(bufferSolicitud);
+static uint32_t __solicitar(int toSocket, uint32_t numeroDeTabla, uint32_t entradaDeTabla, t_header requestHeader) {
+    t_buffer *requestBuffer = buffer_create();
+    buffer_pack(requestBuffer, &numeroDeTabla, sizeof(numeroDeTabla));
+    buffer_pack(requestBuffer, &entradaDeTabla, sizeof(entradaDeTabla));
+    stream_send_buffer(toSocket, requestHeader, requestBuffer);
+    buffer_destroy(requestBuffer);
 
-    uint32_t headerRta = stream_recv_header(toSocket);
-    t_buffer *bufferRta = buffer_create();
-    stream_recv_buffer(toSocket, bufferRta);
-    uint32_t rta;
-    if (headerRta != header) {
+    uint32_t responseHeader = stream_recv_header(toSocket);
+    if (responseHeader != requestHeader) {
         log_error(cpuLogger, "Error al realizar una solicitud de memoria");
         exit(-1);
     }
-    buffer_unpack(bufferRta, &rta, sizeof(rta));
-    buffer_destroy(bufferRta);
-    return rta;
+
+    t_buffer *responseBuffer = buffer_create();
+    stream_recv_buffer(toSocket, responseBuffer);
+
+    responseHeader = -1;
+    buffer_unpack(responseBuffer, &responseHeader, sizeof(responseHeader));
+    buffer_destroy(responseBuffer);
+
+    return responseHeader;
 }
 
-uint32_t obtener_marco(int toSocket, uint32_t direccion, uint32_t nroDeTabla1) {
-    toSocket = cpu_config_get_socket_memoria(cpuConfig);
-    int tamPagina = cpu_config_get_tamanio_pagina(cpuConfig);
+static uint32_t __obtener_marco(int toSocket, uint32_t direccion, uint32_t numberoDeTabla1) {
+    int tamanioPagina = cpu_config_get_tamanio_pagina(cpuConfig);
     int entradasPorTabla = cpu_config_get_entradas_por_tabla(cpuConfig);
 
-    uint32_t nroPag = direccion / tamPagina;
-    uint32_t entradaTablaNivel1 = nroPag / entradasPorTabla;
-    uint32_t entradaTablaNivel2 = nroPag % entradasPorTabla;
-    uint32_t offset = direccion - nroPag * tamPagina;
+    uint32_t numeroPagina = direccion / tamanioPagina;
+    uint32_t entradaTablaNivel1 = numeroPagina / entradasPorTabla;
+    uint32_t entradaTablaNivel2 = numeroPagina % entradasPorTabla;
+    uint32_t offset = direccion - numeroPagina * tamanioPagina;
 
-    uint32_t nroDeTabla2 = solicitar(toSocket, nroDeTabla1, entradaTablaNivel1, HEADER_tabla_nivel_2);
-    uint32_t marco = solicitar(toSocket, nroDeTabla2, entradaTablaNivel2, HEADER_marco);
+    uint32_t numeroDeTabla2 = __solicitar(toSocket, numberoDeTabla1, entradaTablaNivel1, HEADER_tabla_nivel_2);
+    uint32_t marco = __solicitar(toSocket, numeroDeTabla2, entradaTablaNivel2, HEADER_marco);
 
     return marco + offset;
 }
 
-void escribir_en_memoria(int toSocket, uint32_t direccion, uint32_t nroDeTabla1, uint32_t valor) {
-    int marco = obtener_marco(toSocket, direccion, nroDeTabla1);
+void escribir_en_memoria(int toSocket, uint32_t direccionAEscribir, uint32_t numberoDeTabla1, uint32_t contenidoAEscribir) {
+    int marco = __obtener_marco(toSocket, direccionAEscribir, numberoDeTabla1);
 
     t_buffer *buffer = buffer_create();
     uint32_t marcoSend = marco;
     buffer_pack(buffer, &marcoSend, sizeof(marcoSend));
-    buffer_pack(buffer, &valor, sizeof(valor));
+    buffer_pack(buffer, &contenidoAEscribir, sizeof(contenidoAEscribir));
     stream_send_buffer(toSocket, HEADER_write, buffer);
     buffer_destroy(buffer);
 }
 
-uint32_t leer_en_memoria(int toSocket, uint32_t direccion, uint32_t nroDeTabla1) {
-    int marco = obtener_marco(toSocket, direccion, nroDeTabla1);
+uint32_t leer_en_memoria(int toSocket, uint32_t direccionALeer, uint32_t numberoDeTabla1) {
+    int marco = __obtener_marco(toSocket, direccionALeer, numberoDeTabla1);
 
-    t_buffer *buffer = buffer_create();
-    uint32_t marcoSend = marco;
-    buffer_pack(buffer, &marcoSend, sizeof(marcoSend));
-    stream_send_buffer(toSocket, HEADER_write, buffer);
-    buffer_destroy(buffer);
+    t_buffer *requestBuffer = buffer_create();
+    uint32_t marcoAEnviar = marco;
+    buffer_pack(requestBuffer, &marcoAEnviar, sizeof(marcoAEnviar));
+    stream_send_buffer(toSocket, HEADER_write, requestBuffer);
+    buffer_destroy(requestBuffer);
 
-    uint32_t header = stream_recv_header(toSocket);
-    if (header != HEADER_read) {
+    uint32_t responseHeader = stream_recv_header(toSocket);
+    if (responseHeader != HEADER_read) {
         log_error(cpuLogger, "Error al leer en memoria");
         exit(-1);
     }
-    t_buffer *bufferRecv = buffer_create();
-    uint32_t valor;
-    buffer_unpack(bufferRecv, &valor, sizeof(valor));
-    buffer_destroy(bufferRecv);
-    return valor;
-}
-
-void copiar_en_memoria(int toSocket, uint32_t direccionDestino, uint32_t direccionOrigen, uint32_t nroDeTabla1) {
-    int marcoDestino = obtener_marco(toSocket, direccionDestino, nroDeTabla1);
-    int marcoOrigen = obtener_marco(toSocket, direccionOrigen, nroDeTabla1);
-
-    t_buffer *buffer = buffer_create();
-    uint32_t marcoDestSend = marcoDestino;
-    uint32_t marcoOrigenSend = marcoOrigen;
-    buffer_pack(buffer, &marcoDestSend, sizeof(marcoDestSend));
-    buffer_pack(buffer, &marcoOrigenSend, sizeof(marcoOrigenSend));
-    stream_send_buffer(toSocket, HEADER_copy, buffer);
-    buffer_destroy(buffer);
+    t_buffer *responseBuffer = buffer_create();
+    uint32_t contenidoLeido = -1;
+    buffer_unpack(responseBuffer, &contenidoLeido, sizeof(contenidoLeido));
+    buffer_destroy(responseBuffer);
+    return contenidoLeido;
 }
