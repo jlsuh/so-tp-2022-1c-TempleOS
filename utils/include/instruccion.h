8a206533f328f7547525c738edb89ff7d7af43e9
jlsuh <joelleonardosuh@gmail.com>
2022-06-11 15:50:20 -0300

    refactor(utils): instruction toString representation

--- utils/include/instruccion.h
+++ utils/include/instruccion.h
@@ -10,7 +10,7 @@
 
 typedef struct t_instruccion t_instruccion;
 
-char* instruccion_to_string(t_instruccion*);
+char const* instruccion_to_string(t_instruccion*);
 t_instruccion* instruccion_create(t_tipo_instruccion, uint32_t operando1, uint32_t operando2);
 t_list* instruccion_list_create_from_buffer(t_buffer*, t_log*);
 t_tipo_instruccion instruccion_get_tipo_instruccion(t_instruccion*);
