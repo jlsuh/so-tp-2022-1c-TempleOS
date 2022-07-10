d33479d196af911108292a28dcd9ca3902e16685
jlsuh <joelleonardosuh@gmail.com>
2022-07-10 04:29:46 -0300

    chore: deploy script

--- deploy.sh
+++ deploy.sh
@@ -1,142 +1,61 @@
 #!/usr/bin/env bash
 
-cd() { command cd "$@" && printf 'Changing directory: %s -> %s\n' "${OLDPWD}" "${PWD}"; }
+declare -r RULE="release"
+declare -r GREEN="\e[1;92m"
+declare -r ENDCOLOR="\e[0m"
+declare -r COMMONS="so-commons-library"
+declare -r DEPENDENCIES=("utils")
+declare -r PROJECTS=("consola" "kernel" "memoria" "cpu")
 
-bold=$(tput bold)
-normal=$(tput sgr0)
-underline=$(tput smul)
-nounderline=$(tput rmul)
+function cd() { command cd "$@" && printf 'Changing directory: %s -> %s\n' "${OLDPWD}" "${PWD}"; }
 
-if [[ "$@" =~ (-h|-H|--help) ]]; then
-    echo "
-${bold}NAME${normal}
-    ${bold}deploy.sh${normal} - script to deploy sisoputnfrba's TP.
-${bold}SYNOPSIS${normal}
-    ${bold}deploy.sh${normal} [${bold}--lib${normal}=${underline}library${nounderline}] [${bold}--dependency${normal}=${underline}dependency${nounderline}] [${bold}--project${normal}=${underline}project${nounderline}] ${underline}repository${nounderline}
-${bold}DESCRIPTION${normal}
-    The ${bold}deploy.sh${normal} utility is to ease the deploy process.
-    The options are as follows:
-    ${bold}-t | --target${normal}       Changes the directory where the script is executed. By default it will be the current directory.
-    ${bold}-m | --make${normal}         Changes the makefile rule for building projects. By default it will be empty.
-    ${bold}-l | --lib${normal}          Adds an external dependency to build and install.
-    ${bold}-d | --dependency${normal}   Adds an internal dependency to build and install from the repository.
-    ${bold}-p | --project${normal}      Adds a project to build from the repository.
-${bold}COMPATIBILITY${normal}
-    The project must have the following scructure:
-      project1/
-       ╰─ makefile
-      project2/
-       ╰─ makefile
-      sharedlibrary/
-       ╰─ makefile
-${bold}EXAMPLE${normal}
-      ${bold}deploy.sh${normal} ${bold}-l${normal}=sisoputnfrba/ansisop-parser ${bold}-d${normal}=sockets ${bold}-p${normal}=consola ${bold}-p${normal}=kernel ${bold}-p${normal}=memoria ${underline}tp-2017-1C-exampleRepo${nounderline}
-  "
-    exit
-fi
+function install_commons_library() {
+    echo -e "${GREEN}Installing commons libraries...${ENDCOLOR}\n"
+    rm -rf $COMMONS
+    git clone "https://${TOKEN}@github.com/sisoputnfrba/${COMMONS}.git" $COMMONS
+    cd $COMMONS || exit
+    sudo make uninstall
+    make all
+    sudo make install
+    cd "$CWD" || exit
+}
 
-echo -n "Personal Access Token: "
-read -r TOKEN
-
-CWD=$PWD
-case $1 in
--t=* | --target=*)
-    case ${1#*=} in
-    /*) CWD="${1#*=}" ;;
-    *) CWD+="/${1#*=}" ;;
-    esac
-    shift
-    ;;
-*) ;;
-
-esac
-cd "$CWD" || exit
-
-RULE=""
-case $1 in
--m=* | --make=*)
-    RULE="${1#*=}"
-    shift
-    ;;
-*) ;;
-
-esac
-
-echo -e "\n\nInstalling commons libraries...\n\n"
-
-COMMONS="so-commons-library"
-
-rm -rf $COMMONS
-git clone "https://${TOKEN}@github.com/sisoputnfrba/${COMMONS}.git" $COMMONS
-cd $COMMONS || exit
-sudo make uninstall
-make all
-sudo make install
-cd "$CWD" || exit
-
-length=$(($# - 1))
-OPTIONS=${@:1:length}
-
-LIBRARIES=()
-DEPENDENCIES=()
-PROJECTS=()
-
-for i in $OPTIONS; do
-    case $i in
-    -l=* | --lib=*)
-        LIBRARIES+=("${i#*=}")
-        ;;
-    -d=* | --dependency=*)
-        DEPENDENCIES+=("${i#*=}")
-        ;;
-    -p=* | --project=*)
-        PROJECTS+=("${i#*=}")
-        ;;
-    *) ;;
-
-    esac
-done
-
-if [ ${#LIBRARIES[@]} -ne 0 ]; then
-    echo -e "\n\nCloning external libraries..."
-    for i in "${LIBRARIES[@]}"; do
-        echo -e "\n\nBuilding ${i}\n\n"
-        rm -rf "$i"
-        git clone "https://${TOKEN}@github.com/${i}.git" "$i"
-        cd "$i" || exit
-        make install
-        cd "$CWD" || exit
-    done
-else
-    echo -e "\n\nNo external libraries were indicated..."
-fi
-
-if [ ${#DEPENDENCIES[@]} -ne 0 ]; then
-    echo -e "\n\nBuilding dependencies..."
+function build_dependencies() {
+    echo -e "${GREEN}Building dependencies...${ENDCOLOR}\n"
     for i in "${DEPENDENCIES[@]}"; do
-        echo -e "\n\nBuilding ${i}\n\n"
+        echo -e "${GREEN}Building dependency ${i}${ENDCOLOR}\n"
         cd "$i" || exit
         make install
         cd "$CWD" || exit
     done
-else
-    echo -e "\n\nNo dependencies were indicated..."
-fi
+}
 
-if [ ${#PROJECTS[@]} -ne 0 ]; then
-    echo -e "\n\nBuilding projects..."
+function build_projects() {
+    echo -e "${GREEN}Building projects...${ENDCOLOR}\n"
     for i in "${PROJECTS[@]}"; do
-        echo -e "\n\nBuilding ${i}\n\n"
+        echo -e "${GREEN}Building project ${i}${ENDCOLOR}\n"
         cd "$i" || exit
         make "$RULE"
         cd "$CWD" || exit
     done
-else
-    echo -e "\n\nNo projects were indicated..."
-fi
+}
 
-cd ..
-sudo mkdir "swap/" || exit
-echo -e "\n\nSwap subdirectory created...\n\n"
+function main() {
+    local -r CWD=$PWD
 
-echo -e "\n\nDeploy finished\n\n"
+    echo -n "Personal Access Token: "
+    read -r TOKEN
+
+    install_commons_library
+    build_dependencies
+    build_projects
+
+    cd ..
+    sudo mkdir "swap/" || exit
+    sudo chmod 777 "swap/" || exit
+    echo -e "${GREEN}Swap subdirectory created...${ENDCOLOR}\n"
+
+    echo -e "${GREEN}Deploy finished${ENDCOLOR}\n"
+}
+
+main
