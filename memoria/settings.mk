d567bdbb98448a85a572565bf5d0105d22c492bf
jlsuh <joelleonardosuh@gmail.com>
2022-07-14 21:28:54 -0300

    fix

--- memoria/settings.mk
+++ memoria/settings.mk
@@ -10,7 +10,7 @@ CDEBUG=-g -Wall -DDEBUG
 CRELEASE=-O3 -Wall -DNDEBUG
 
 # Arguments when executing with start, memcheck or helgrind
-ARGS=cfg/memoria_config.cfg
+ARGS=cfg/prueba_estabilidad_integral_config.cfg
 
 # Valgrind flags
 MEMCHECK_FLAGS=--track-origins=yes --log-file="memcheck.log"
--- memoria/src/atender_kernel.c
+++ memoria/src/atender_kernel.c
@@ -78,9 +78,9 @@ void* escuchar_peticiones_kernel(void* socketKernel) {
                 log_info(memoriaData->memoriaLogger, "\e[1;93mSe suspende proceso\e[0m");
                 buffer_unpack(buffer, &nroTablaNivel1, sizeof(nroTablaNivel1));
 
+                log_info(memoriaData->memoriaLogger, "Se suspendio tabla de nivel 1 con ID [%d]", nroTablaNivel1);
                 suspender_proceso(nroTablaNivel1, memoriaData);
 
-                log_info(memoriaData->memoriaLogger, "Se suspendio tabla de nivel 1 con ID [%d]", nroTablaNivel1);
                 stream_send_empty_buffer(socket, HANDSHAKE_ok_continue);
                 buffer_destroy(buffer);
                 break;
