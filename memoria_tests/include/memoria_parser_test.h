4b849dca2e917bc5b1c5866c6c8029f3163e710a
jlsuh <joelleonardosuh@gmail.com>
2022-07-17 16:15:48 -0300

    chore(memoria_tests): clean unused files

--- memoria_tests/include/memoria_parser_test.h
+++
@@ -1,9 +0,0 @@
-#ifndef MEMORIA_PARSER_TESTS_H_INCLUDED
-#define MEMORIA_PARSER_TESTS_H_INCLUDED
-
-void test_memoria_parser_setup(void);
-void test_memoria_parser_tear_down(void);
-
-void test_se_crea_correctamente(void);
-
-#endif
--- memoria_tests/src/memoria_tests.c
+++ memoria_tests/src/memoria_tests.c
@@ -1,31 +1,10 @@
-#include <stdio.h>
-#include <stdbool.h>
-#include <CUnit/Basic.h>
+#include <pthread.h>
 
-#include "memoria_parser_test.h"
-#include "testing_utils.h"
+#include "memoria_data_holder.h"
 
-static void memoria_tests(void) {
-    CU_pSuite memoriaParserSuite = CU_add_suite_with_setup_and_teardown(
-        "memoria_parser.h Test Suite",
-        NULL,
-        NULL,
-        test_memoria_parser_setup,
-        test_memoria_parser_tear_down);
-    t_test_case memoriaParserTestCases[] = {
-        TEST_FUNC(test_se_crea_correctamente),
-    };
-    ADD_TEST_CASES_TO_SUITE(memoriaParserSuite, memoriaParserTestCases);
-}
+t_memoria_data_holder* memoriaData;
+pthread_mutex_t mutexMemoriaData;
 
 int main(int argc, char* argv[]) {
-    CU_initialize_registry();
-    CU_basic_set_mode(CU_BRM_VERBOSE);
-
-    memoria_tests();
-
-    CU_basic_run_tests();
-    CU_cleanup_registry();
-
-    return CU_get_error();
+    return 0;
 }
