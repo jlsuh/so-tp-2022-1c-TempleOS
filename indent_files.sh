b4c34e6995334022be61096d14db72e29e3063c6
jlsuh <joelleonardosuh@gmail.com>
2022-07-17 04:10:44 -0300

    chore: print indentation

--- indent_files.sh
+++ indent_files.sh
@@ -4,5 +4,5 @@ readarray -t array < <(find . -name \*.h -print -o -name \*.c -print)
 
 for i in "${array[@]}"; do
     echo -e "\n\e[1;92m>>>>>>>>>>>>>>>>>>>> Indenting ($i) <<<<<<<<<<<<<<<<<<<<\e[0m"
-    clang-format -style=file "$i" "$i"
+    clang-format -style=file "$i"
 done
